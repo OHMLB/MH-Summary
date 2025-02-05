9f0ac5ff494008ab9994cfd22ffb0499d1a558e6
OHMLB <sukit.sup@gmail.com>
2025-02-05 20:12:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -276,11 +276,11 @@ def main():
   
     # Store file paths in session state  
     if uploaded_file_1 is not None:  
-        actual_path = uploaded_file_1.name
+        actual_path = uploaded_file_1
         st.session_state.file_paths['actual'].append(actual_path)  
   
     if uploaded_file_2 is not None:  
-        estimated_path = uploaded_file_2.name
+        estimated_path = uploaded_file_2
         st.session_state.file_paths['estimated'].append(estimated_path)  
     print(st.session_state.file_paths)
     # Dropdown selector to select from previously uploaded files  
